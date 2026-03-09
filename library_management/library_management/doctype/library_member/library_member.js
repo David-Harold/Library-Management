497e04901fc02dd0e105f070c9588051a82459ee
David-Harold <ke.d08@yahoo.cm>
2026-03-09 19:43:39 +0000

    feat: added Author Child Table

--- library_management/library_management/doctype/library_member/library_member.js
+++ library_management/library_management/doctype/library_member/library_member.js
@@ -1,8 +1,26 @@
 // Copyright (c) 2026, David-Harold Essiben and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Library Member", {
-// 	refresh(frm) {
+frappe.ui.form.on("Library Member", {
+	onload(frm) {
+        frappe.msgprint(`Hey ${frm.doc.first_name} ${frm.doc.last_name}, welcome to the library!`);
 
-// 	},
-// });
+	},
+	first_name(frm){
+		first_name = frm.doc.first_name
+	    last_name = frm.doc.last_name
+	    full_name = `${first_name} ${last_name}`
+	    frm.set_value("full_name", full_name)
+    },
+	last_name(frm) {
+		first_name = frm.doc.first_name
+	    last_name = frm.doc.last_name
+	    full_name = `${first_name} ${last_name}`
+	    frm.set_value("full_name", full_name)
+
+	},
+	after_save(frm) {
+		frappe.msgprint(`${frm.doc.full_name}, Duely Saved!`);
+	}
+
+});
